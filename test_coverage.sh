7979009c2db43b065f2c8c69fa267e6c51d33a4e
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-02 13:46:01 +0530

    ci file changed

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir -p artifacts
+# mkdir -p artifacts
 
-flutter analyze > artifacts/analyze_artifacts.txt  
+# flutter analyze > artifacts/analyze_artifacts.txt  
 
-echo "analyze done"
+# echo "analyze done"
 
-flutter pub global run dart_code_metrics:metrics lib --reporter=json > artifacts/code_metrics_artifacts.txt  
+# flutter pub global run dart_code_metrics:metrics lib --reporter=json > artifacts/code_metrics_artifacts.txt  
 
 flutter test --coverage  && genhtml coverage/lcov.info -o coverage/html > artifacts/coverage_artifacts.txt
 
