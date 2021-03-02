a56bd79e223072c04e6899d39f656f4e4de0b7ba
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-02 23:40:52 +0530

    firebase test lab test

--- firebase_test_lab.sh
+++ firebase_test_lab.sh
@@ -18,3 +18,6 @@ ls
 cd build/Build/Products 
 
 zip -r MyTests.zip Debug-iphoneos  Runner_iphoneos14.4-arm64.xctestrun
+
+gcloud firebase test ios run --test MyTests.zip \
+ --device model=iphonex,version=11.4,locale=en_US,orientation=portrait
