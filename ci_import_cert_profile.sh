3f348ba1b5f51beaba99d3183a88e81381d537e1
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-08 22:01:21 +0530

    security certificate added

--- ci_import_cert_profile.sh
+++ ci_import_cert_profile.sh
@@ -2,7 +2,7 @@ cd ios
 
 security add-certificates AppleWWDRCAG3.cer
 
-security import apple_developement_certificate.p12 -P bbii -T /usr/bin/codesign
+security import apple_developement_certificate.p12 -P bbii -T /usr/bin/codesign -A
 
 mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
 
