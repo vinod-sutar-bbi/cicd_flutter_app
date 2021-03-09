30b0e6a87ebc939e2bdb1f0ef2822ccee125002d
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-09 18:41:17 +0530

    security certificate added

--- ci_import_cert_profile.sh
+++ ci_import_cert_profile.sh
@@ -7,4 +7,5 @@ security import apple_developement_certificate.p12 -P bbii -T /usr/bin/codesign 
 mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
 
 uuid=`grep UUID -A1 -a adhoc.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
+
 cp "adhoc.mobileprovision" "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
