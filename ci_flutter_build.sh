ab4599569bbdf60a7b9211f1b8e9f9998abe4171
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-11 14:43:40 +0530

    security certificate added

--- ci_flutter_build.sh
+++ ci_flutter_build.sh
@@ -2,25 +2,27 @@
 
 # flutter build ios integration_test/custom_integration_test.dart
 
-# cd ios
+cd ios
 
-# xcodebuild -workspace Runner.xcworkspace \
-# -derivedDataPath build \
-# -scheme Runner \
-# -sdk iphoneos build-for-testing
+xcodebuild -workspace Runner.xcworkspace \
+-derivedDataPath build \
+-scheme Runner \
+-sdk iphoneos build-for-testing \
+CODE_SIGN_IDENTITY="Apple Development: BB Developer (QXZZ2XV3GC)"
+PROVISIONING_PROFILE="45407bcc-382c-4cc0-94c3-2fef0bb2e2c1"
 
-flutter clean
+# flutter clean
 
-pushd android
+# pushd android
 
-flutter build apk
-./gradlew app:assembleAndroidTest
-./gradlew app:assembleDebug -Ptarget=integration_test/custom_integration_test.dart
-popd
+# flutter build apk
+# ./gradlew app:assembleAndroidTest
+# ./gradlew app:assembleDebug -Ptarget=integration_test/custom_integration_test.dart
+# popd
 
-gcloud firebase test android run \
-  --type robo \
-  --app build/app/outputs/flutter-apk/app-release.apk \
-  --device model=Nexus6,version=21,locale=en,orientation=portrait  \
-  --device model=Nexus7,version=19,locale=fr,orientation=landscape \
-  --timeout 90s
+# gcloud firebase test android run \
+#   --type robo \
+#   --app build/app/outputs/flutter-apk/app-release.apk \
+#   --device model=Nexus6,version=21,locale=en,orientation=portrait  \
+#   --device model=Nexus7,version=19,locale=fr,orientation=landscape \
+#   --timeout 90s
