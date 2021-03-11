58851963c8e82ce968323ed2d1761f532fcd3df1
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-11 12:30:11 +0530

    security certificate added

--- ci_import_cert_profile.sh
+++ ci_import_cert_profile.sh
@@ -9,3 +9,5 @@ mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
 uuid=`grep UUID -A1 -a adhoc.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
 
 cp "adhoc.mobileprovision" "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
+
+ls "$HOME/Library/MobileDevice/Provisioning Profiles"
