10a675191c6a74131f1150e54b8f687c38465bba
Vinod Sutar <vinod.sutar@media4u.com>
2021-03-17 16:56:53 +0530

    script changed

--- ci_flutter_build.sh
+++ ci_flutter_build.sh
@@ -19,6 +19,6 @@ pod install --verbose
 xcodebuild -workspace Runner.xcworkspace \
 -derivedDataPath build \
 -scheme Runner \
--sdk iphoneos build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
+-sdk iphoneos build-for-testing SDKROOT="iphoneos14.2" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_ENTITLEMENTS="" CODE_SIGNING_ALLOWED="NO"
 
 ls build/Build/Products
